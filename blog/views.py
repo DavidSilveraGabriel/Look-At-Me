8f9490a43137edf009b07a1d1d85f758a9ea33a8
DavidSilveraGabriel <ingenieria.d.s.g@hotmail.com>
2020-12-21 02:49:59 +0100

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post #el punto indica que esta en el directorio actual
+from django.utils import timezone
 
 # Create your views here.
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+def post_list(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_list.html', {'posts': posts})
